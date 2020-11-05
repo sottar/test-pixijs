4a6a7380342e41a5ee561d5aeee8a5dac47e2acb
sottar <ohrst.18@gmail.com>
2020-11-05 10:50:11 +0900

    add global style

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import { App } from './App';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: "游ゴシック Medium", "Yu Gothic Medium", "游ゴシック体", "YuGothic", "ヒラギノ角ゴ ProN W3", "Hiragino Kaku Gothic ProN", "メイリオ", "Meiryo", "verdana", sans-serif;
+    color: #333;
+    background-color: #fcfcfc;
+    margin: 0;
+    padding: 0;
+  }
+  p {
+    margin: 0;
+  }
+  ul {
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    padding: 0;
+    list-style: none;
+  }
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
